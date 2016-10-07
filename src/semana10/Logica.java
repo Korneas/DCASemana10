6fc01919ef0790eda752941d888e93e8b3ff4ab9
CamiloMontoya <CamiloMontoya@172.30.169.114>
2016-10-07 10:16:06 -0500

    Voids

--- src/semana10/Logica.java
+++ src/semana10/Logica.java
@@ -1,7 +1,16 @@
 package semana10;
 
 public class Logica {
+	
 	public Logica() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void ejecutar(){
+		
+	}
+	
+	public void click(){
+		
 	}
 }
